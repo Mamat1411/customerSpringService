2a991a44682f351da951f4afa069200c0e976c2c
Mamat1411 <muhammadmujahid14@gmail.com>
2024-01-16 21:49:01 +0700

    Customer Entity

---
+++ src/main/java/com/batch1/customerSpringService/customers/entities/Customer.java
@@ -0,0 +1,44 @@
+package com.batch1.customerSpringService.customers.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "customers")
+public class Customer {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name", length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", length = 50)
+    private String lastName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "postal_code", length = 5)
+    private Long postalCode;
+
+    @Column(name = "phone_number", length = 15)
+    private String phoneNumber;
+
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @Column(name = "status", length = 50)
+    private String status;
+
+    @Column(name = "type_of_customer", length = 50)
+    private String typeOfCustomer;
+}
