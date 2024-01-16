5f0cfc4713fad9196df9bcfec98c63e0c9adaa9a
Mamat1411 <muhammadmujahid14@gmail.com>
2024-01-16 21:21:52 +0700

    Initial Commit

---
+++ src/main/java/com/batch1/customerSpringService/CustomerSpringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.batch1.customerSpringService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSpringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSpringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch1/customerSpringService/CustomerSpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch1.customerSpringService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSpringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
